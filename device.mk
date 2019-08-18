dc8c52cea8bde197cac2d7fa27bb21eabb09c250
UtsavisGreat <utsavbalar1231@gmail.com>
2019-08-18 07:43:03 +0000

    raphael: Import and bind audio_effects.xml
    raphaelin-user-9-PKQ1.181121.001-V10.3.3.0.PFKINXM-release-keys
    
    Signed-off-by: UtsavisGreat <utsavbalar1231@gmail.com>

--- device.mk
+++ device.mk
@@ -30,7 +30,8 @@ PRODUCT_PACKAGES += \
     audio.a2dp.default
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/audio/audio_policy_configuration.xml:system/etc/audio_policy_configuration.xml
+    $(LOCAL_PATH)/audio/audio_policy_configuration.xml:system/etc/audio_policy_configuration.xml \
+    $(LOCAL_PATH)/audio/audio_effects.xml:system/etc/audio_effects.xml
 
 # Bluetooth
 PRODUCT_PACKAGES += \
